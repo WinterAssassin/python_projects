11a48ec8b37da0acb5e7ed55c6a0fc2f9742a179
MJ Cabrera <mjcabrera1996@gmail.com>
2024-04-22 16:20:57 +0800

    Upload selenium scraper projects

--- selenium/main.py
+++ selenium/main.py
@@ -1,15 +1,22 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+import time
+
+
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_experimental_option("detach", True)
+# chrome_options.add_experimental_option("detach", True)
 
 # chrome_driver_path = "C:/Program Files (x86)/chromedriver.exe"
 # service = Service(executable_path=chrome_driver_path)
 
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://www.google.com/")
+driver.get("http://ph.indeed.com/")
 
 # while True:
 #     pass
+
+time.sleep(10)
+
+driver.quit() 
+
